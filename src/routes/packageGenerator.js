4db2aadffed0ed871a98de2b52b18cfc7dde5617
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-12-13 16:19:40 +0100

    new

--- src/routes/packageGenerator.js
+++ src/routes/packageGenerator.js
@@ -1,9 +1,27 @@
+// import express from 'express';
+// import { createShipment } from '../controllers/createPackageGenerator.js';
+
+
+
+// const router = express.Router();
+
+// router.post('/', createShipment); // Створення нової посилки
+// export default router;
 import express from 'express';
 import { createShipment } from '../controllers/createPackageGenerator.js';
-
-
+import { addToQueue } from '../services/queue/queueProcessor.js';
+ // Подключаем вашу очередь
 
 const router = express.Router();
 
-router.post('/', createShipment); // Створення нової посилки
+// Используем очередь для маршрута
+router.post('/', (req, res, next) => {
+  addToQueue({
+    handler: createShipment, // Передаем ваш контроллер
+    req, // Текущий запрос
+    res, // Ответ
+    next, // Следующий обработчик
+  });
+});
+
 export default router;
