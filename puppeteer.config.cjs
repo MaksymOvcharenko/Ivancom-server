b32b3bb1e3124f8b0cb21a7158f110af7b6d51cb
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2025-08-26 14:19:03 +0200

    туц

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -7,6 +7,3 @@
 //     // Changes the cache location for Puppeteer.
 //     cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 // };
-// "puppeteer": "^23.10.3",
-//     "puppeteer-core": "^23.10.3",
-//         в пакадж жсон добавить
